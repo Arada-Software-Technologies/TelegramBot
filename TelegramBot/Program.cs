539cb9e17bbf51554646377588355d153b8c0013
AmanuelKefelegne <demissieamanuel92@gmail.com>
2020-12-23 10:14:47 +0300

    add Telegram token

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Telegram.Bot;
 
 namespace TelegramBot
 {
     class Program
     {
+        private static readonly TelegramBotClient bot = new TelegramBotClient("1428816140:AAHZGqsBzRkQ_MEapfWgbReIUyjxuavedLo");
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
